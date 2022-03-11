4ef4575a9deb38df962c0f152dcaef6a10c46147
Niamh Belton <niamh.belton@ucdconnect.ie>
2022-03-11 14:23:36 +0000

    ae

---
+++ src/ae.py
@@ -0,0 +1,72 @@
+import json
+import torch
+
+from base.base_dataset import BaseADDataset
+from networks.main import build_network, build_autoencoder
+from optim.deepSVDD_trainer import DeepSVDDTrainer
+from optim.ae_trainer import AETrainer
+
+
+class ae(object):
+    """A class for the Deep SVDD method.
+
+    Attributes:
+        objective: A string specifying the Deep SVDD objective (either 'one-class' or 'soft-boundary').
+        nu: Deep SVDD hyperparameter nu (must be 0 < nu <= 1).
+        R: Hypersphere radius R.
+        c: Hypersphere center c.
+        net_name: A string indicating the name of the neural network to use.
+        net: The neural network \phi.
+        ae_net: The autoencoder network corresponding to \phi for network weights pretraining.
+        trainer: DeepSVDDTrainer to train a Deep SVDD model.
+        optimizer_name: A string indicating the optimizer to use for training the Deep SVDD network.
+        ae_trainer: AETrainer to train an autoencoder in pretraining.
+        ae_optimizer_name: A string indicating the optimizer to use for pretraining the autoencoder.
+        results: A dictionary to save the results.
+    """
+
+    def __init__(self, objective: str = 'one-class', nu: float = 0.1):
+        """Inits DeepSVDD with one of the two objectives and hyperparameter nu."""
+
+        assert objective in ('one-class', 'soft-boundary'), "Objective must be either 'one-class' or 'soft-boundary'."
+        self.objective = objective
+        assert (0 < nu) & (nu <= 1), "For hyperparameter nu, it must hold: 0 < nu <= 1."
+        self.nu = nu
+        self.R = 0.0  # hypersphere radius R
+        self.c = None  # hypersphere center c
+
+        self.net_name = None
+        self.net = None  # neural network \phi
+
+        self.trainer = None
+        self.optimizer_name = None
+
+        self.ae_net = None  # autoencoder network for pretraining
+        self.ae_trainer = None
+        self.ae_optimizer_name = None
+
+        self.results = {
+            'train_time': None,
+            'test_auc': None,
+            'test_time': None,
+            'test_scores': None,
+        }
+
+    def set_network(self, net_name):
+        """Builds the neural network \phi."""
+        self.net_name = net_name
+        self.net = build_network(net_name)
+
+    def pretrain(self, dataset: BaseADDataset, optimizer_name: str = 'adam', lr: float = 0.001, n_epochs: int = 100,
+                 lr_milestones: tuple = (), batch_size: int = 128, weight_decay: float = 1e-6, device: str = 'cuda',
+                 n_jobs_dataloader: int = 0):
+        """Pretrains the weights for the Deep SVDD network \phi via autoencoder."""
+
+        self.ae_net = build_autoencoder(self.net_name)
+        self.ae_optimizer_name = optimizer_name
+        self.ae_trainer = AETrainer(optimizer_name, lr=lr, n_epochs=n_epochs, lr_milestones=lr_milestones,
+                                    batch_size=batch_size, weight_decay=weight_decay, device=device,
+                                    n_jobs_dataloader=n_jobs_dataloader)
+        self.ae_net = self.ae_trainer.train(dataset, self.ae_net)
+        self.ae_trainer.test(dataset, self.ae_net)
+        self.init_network_weights_from_pretraining()
